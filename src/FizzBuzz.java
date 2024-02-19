33dc864a40c85866111fa8d409fe0d07d9a6e5df
digiwrite919 <159510585+digiwrite919@users.noreply.github.com>
2024-02-19 11:29:27 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,9 +1,16 @@
-
 public class FizzBuzz {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!");
-	}
-
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      if (i % 15 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      } else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
 }
